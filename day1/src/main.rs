d5b6d78868677c026ebe3fcd755d4212223026fe
Martin Kröner <work@kroner.dev>
2023-12-03 15:41:30 +0100

    day1: part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,17 +1,68 @@
+use regex::Regex;
+
+fn word_to_num(w: &str) -> &str {
+    match w {
+        "one" => "1",
+        "two" => "2",
+        "three" => "3",
+        "four" => "4",
+        "five" => "5",
+        "six" => "6",
+        "seven" => "7",
+        "eight" => "8",
+        "nine" => "9",
+        _ => unreachable!(),
+    }
+}
+
 fn main() {
     let stdin = std::io::stdin();
     let mut lines = stdin.lines();
 
+    let regexes = [
+        Regex::new(r"1").unwrap(),
+        Regex::new(r"2").unwrap(),
+        Regex::new(r"3").unwrap(),
+        Regex::new(r"4").unwrap(),
+        Regex::new(r"5").unwrap(),
+        Regex::new(r"6").unwrap(),
+        Regex::new(r"7").unwrap(),
+        Regex::new(r"8").unwrap(),
+        Regex::new(r"9").unwrap(),
+        Regex::new("one").unwrap(),
+        Regex::new("two").unwrap(),
+        Regex::new("three").unwrap(),
+        Regex::new("four").unwrap(),
+        Regex::new("five").unwrap(),
+        Regex::new("six").unwrap(),
+        Regex::new("seven").unwrap(),
+        Regex::new("eight").unwrap(),
+        Regex::new("nine").unwrap(),
+    ];
+
     let mut sum = 0;
     while let Some(Ok(line)) = lines.next() {
-        let i = line.chars().find(|c| c.is_numeric());
-        let j = line.chars().rev().find(|c| c.is_numeric());
+        let mut matches = regexes
+            .iter()
+            .map(|rgx| rgx.find_iter(&line))
+            .flat_map(|x| x)
+            .collect::<Vec<_>>();
+        matches.sort_by_key(|x| x.start());
+        let first = matches.iter().next().unwrap().as_str();
+        let last = matches.iter().last().unwrap().as_str();
+        let first = if first.len() > 1 {
+            word_to_num(first)
+        } else {
+            first
+        };
+        let last = if last.len() > 1 {
+            word_to_num(last)
+        } else {
+            last
+        };
         let mut s = String::new();
-        if i.is_none() || j.is_none() {
-            continue;
-        }
-        s.push(i.unwrap());
-        s.push(j.unwrap());
+        s.push_str(first);
+        s.push_str(last);
         sum += s.parse::<u128>().unwrap();
     }
 
