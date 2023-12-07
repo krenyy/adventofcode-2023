97f56e9196f3430c475a2289083b2e05c3e31f5f
Martin Kröner <gitlab@kroner.dev>
2023-12-08 00:49:18 +0100

    day4: part2 (shitty complexity)

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,9 +1,17 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
+
+#[derive(Clone, Debug)]
+struct Card {
+    id: u128,
+    winning_numbers: HashSet<u128>,
+    numbers: Vec<u128>,
+}
 
 fn main() {
     let mut lines = std::io::stdin().lines();
 
-    let mut sum = 0;
+    let mut cards = vec![];
+    let mut id = 1;
     while let Some(Ok(line)) = lines.next() {
         let v = line.split(" | ").collect::<Vec<_>>();
         let (first, second) = (v[0], v[1]);
@@ -15,24 +23,36 @@ fn main() {
             .map(|x| x.parse::<u128>().unwrap())
             .collect::<HashSet<_>>();
 
-        let hand = second
+        let numbers = second
             .split(' ')
             .filter(|x| !x.is_empty())
             .map(|x| x.parse::<u128>().unwrap())
             .collect::<Vec<_>>();
 
-        let mut card_value = 0;
-        for num in hand {
-            if winning_numbers.contains(&num) {
-                if card_value == 0 {
-                    card_value = 1;
-                } else {
-                    card_value *= 2;
-                }
-            }
-        }
-        sum += card_value;
+        cards.push(Card {
+            id,
+            winning_numbers,
+            numbers,
+        });
+        id += 1;
     }
 
-    println!("{sum}");
+    let mut card_queue = VecDeque::from(cards.iter().collect::<Vec<_>>());
+
+    let mut card_count = 0;
+    while !card_queue.is_empty() {
+        let card = card_queue.pop_front().unwrap();
+        let x = card
+            .numbers
+            .iter()
+            .map(|n| card.winning_numbers.contains(&n))
+            .filter(|a| *a)
+            .count();
+        for i in card.id..(card.id + x as u128) {
+            card_queue.push_back(&cards[i as usize]);
+        }
+        card_count += 1;
+    }
+
+    println!("{card_count}");
 }
